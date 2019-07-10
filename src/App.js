e7cf219083a87ca3e4f3b7ce548b56ba94cee95d
mochibot <amusaur@gmail.com>
2019-07-10 12:30:45 -0700

    WIP: project meets MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+import Content from './components/Content';
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(response => {
+        console.log('fetch photo success: ', response)
+        setData(response.data)
+      }) 
+      .catch(error => {
+        console.log('error fetching data: ', error)
+        setError(error)
+      })   
+  }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h1>NASA Astronomy Picture of the Day</h1>
+      {!data ? <div>Loading...</div> : <Content data={data}/>}
+      {error && <div>Something went wrong...</div>}
     </div>
   );
 }
---
+++ src/components/Content.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Iframe from 'react-iframe';
+
+const Content = (props) => {
+  return (
+    <div className='App-content'>
+      <div>Title: {props.data.title}</div>
+      <div>Date: {props.data.date}</div>
+      {props.data['media_type'] === 'video' ? <Iframe className='App-content-video'url={props.data.url} width='450px' height='450px'/> : <img className='App-content-image'src={props.data.url} alt='NASA photo of the day'/>}
+      <div>Description: {props.data.explanation}</div>
+    </div>
+
+  )
+}
+
+export default Content; 
