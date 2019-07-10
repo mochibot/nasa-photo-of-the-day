80da388b08b796ac59700a67d26ec7099320cc63
mochibot <amusaur@gmail.com>
2019-07-10 14:17:23 -0700

    WIP: dropdown menu working. can fetch pictures for past 10 days

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import "./App.css";
 function App() {
   const [data, setData] = useState('');
   const [error, setError] = useState('');
+  const [date, setDate] = useState('')
 
   useEffect(() => {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY${date}`)
       .then(response => {
         console.log('fetch photo success: ', response)
         setData(response.data)
@@ -18,11 +19,28 @@ function App() {
         console.log('error fetching data: ', error)
         setError(error)
       })   
-  }, [])
+  }, [date])
+
+  const changeDate = (event) => {
+    setDate('&date=' + event.target.value);
+    console.log(event.target.value);
+  }
+
+  const dateRange = [];
+  for (let i = 10; i >= 1; i--) {
+    let today = new Date();
+    let newDate = new Date(today - i * 24 * 3600 * 1000).toISOString().slice(0, 10)
+    dateRange.unshift(newDate);
+  }
+  dateRange.unshift('today');
 
   return (
     <div className="App">
       <h1>NASA Astronomy Picture of the Day</h1>
+      <select onChange={changeDate} defaultValue={date}>
+        <option disabled>Select a date</option>
+        {dateRange.map(item => item === 'today' ? <option key={item} value=''>{item}</option> : <option key={item} value={item} >{item}</option>)}
+      </select>
       {!data ? <div>Loading...</div> : <Content data={data}/>}
       {error && <div>Something went wrong...</div>}
     </div>
--- src/components/Content.js
+++ src/components/Content.js
@@ -6,7 +6,7 @@ const Content = (props) => {
     <div className='App-content'>
       <div>Title: {props.data.title}</div>
       <div>Date: {props.data.date}</div>
-      {props.data['media_type'] === 'video' ? <Iframe className='App-content-video'url={props.data.url} width='450px' height='450px'/> : <img className='App-content-image'src={props.data.url} alt='NASA photo of the day'/>}
+      {props.data['media_type'] === 'video' ? <Iframe className='App-content-video'url={props.data.url} width='512px' height='384px'/> : <img className='App-content-image'src={props.data.url} alt='NASA photo of the day'/>}
       <div>Description: {props.data.explanation}</div>
     </div>
 
